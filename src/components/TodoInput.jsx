0080511ad27888c4ca841f09e3ee919b1166a7c8
yıldız <mesel4461@gmail.com>
2023-04-23 23:29:15 +0300

    as

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,7 +2,21 @@ import React, { useState } from "react";
 import "./TodoInput.css";
 
 const TodoInput = () => {
-  return <form className="TodoInput">TodoInput</form>;
+  const [task, setTask] = useState();
+
+  return (
+    <form className="TodoInput" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Task"
+        id="task"
+        name="task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      {task}
+    </form>
+  );
 };
 
 export default TodoInput;
